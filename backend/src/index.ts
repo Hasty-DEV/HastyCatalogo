9075a632afe7afe45f5d22b97b6e3e6093588a09
Jefflds <Jeffersonlds3009@gmail.com>
2024-01-10 13:23:35 -0300

    SETUP

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import router from "./routes/main";
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Seja Bem vindo a API do Hasty Catalogo!");
+});
+
+app.use("/api", router);
+
+app.use((req: Request, res: Response) => {
+  res.status(404).send("Endpoint não encontrado");
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send("Erro interno do servidor");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando com sucesso`);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Hello HastyCatalogo</div>;
+}
+
+export default App;
