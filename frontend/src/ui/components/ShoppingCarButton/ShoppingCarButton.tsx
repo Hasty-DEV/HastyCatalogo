c02772b81c635b9ecdd284c4480ce68c742bb41d
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-01-18 22:16:52 -0300

    Estilizando Carrinho de Produtos

--- frontend/src/ui/components/ShoppingCarButton/ShoppingCarButton.tsx
+++ frontend/src/ui/components/ShoppingCarButton/ShoppingCarButton.tsx
@@ -7,7 +7,12 @@ const ShoppingCarButton: React.FC = () => {
 
   const handleOnClick = () => {
     setShowCar(!showCar);
-  }
+  };
+
+  const handleCloseButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setShowCar(false);
+  };
 
   return (
     <>
@@ -16,11 +21,22 @@ const ShoppingCarButton: React.FC = () => {
       </ButtonShoppingCar>
       {showCar ? (
         <ShoppingCar showCar={showCar}>
-          <ShoppingCarContainer>
-            <ButtonShoppingCar title="Carrinho" onClick={handleOnClick}>
-              <IoMdClose />
-            </ButtonShoppingCar>
-            <h3>Carrinho</h3>
+          <ShoppingCarContainer className="d-flex align-items-center justify-content-center flex-column">
+            <ButtonAndTitleContainer className="d-flex">
+              <ButtonShoppingCar
+                title="Fechar Carrinho"
+                onClick={(e) => handleCloseButtonClick(e)}
+              >
+                <IoMdClose />
+              </ButtonShoppingCar>
+              <div className="d-flex align-items-start justify-content-center">
+                <h3>Carrinho</h3>
+              </div>
+              <div></div>
+              <div></div>
+            </ButtonAndTitleContainer>
+            <hr />
+
           </ShoppingCarContainer>
         </ShoppingCar>
       ) : null}
@@ -33,11 +49,16 @@ export default ShoppingCarButton;
 import styled from "styled-components";
 
 export const ButtonShoppingCar = styled.button`
+  background: transparent;
   border: none;
   outline: none;
-  background: transparent;
   padding: 10px;
-  width: 10%;
+  cursor: pointer;
+
+  div {
+    display: inline-block;
+  }
+
   svg {
     width: 25px;
     height: 25px;
@@ -45,24 +66,28 @@ export const ButtonShoppingCar = styled.button`
 `;
 
 export const ShoppingCar = styled.div<{ showCar: boolean }>`
-  position: absolute;
+  position: fixed;
   top: 0;
   right: 0;
   z-index: 2;
   background-color: white;
   height: 100vh;
   padding: 20px;
-  width: 35vw;
-  opacity: ${(props) => (props.showCar ? 1 : 0)};
-  visibility: ${(props) => (props.showCar ? "visible" : "hidden")};
+  width: 40vw;
+  opacity: ${({ showCar }) => (showCar ? 1 : 0)};
+  visibility: ${({ showCar }) => (showCar ? "visible" : "hidden")};
   transition: opacity 0.6s ease, visibility 0.6s ease;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  overflow-y: auto;
+
+  hr {
+    width: 50%;
+  }
 `;
 
 export const ShoppingCarContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
 
   h3 {
     margin-top: 5%;
@@ -71,4 +96,9 @@ export const ShoppingCarContainer = styled.div`
     text-align: center;
     text-transform: uppercase;
   }
+`;
+
+export const ButtonAndTitleContainer = styled.div`
+  justify-content: space-around;
+  width: 100%;
 `;
